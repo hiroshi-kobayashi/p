b79ad2e6848e0e0fdf3be472319446f834059662
hiroshi kobayashi <hiroshi.kobayashi1988@gmail.com>
2018-07-10 18:25:03 +0900

    Create D012.java

---
+++ src/com/d/D012.java
@@ -0,0 +1,17 @@
+package com.d;
+
+import java.util.*;
+
+/**
+ * 絶対値を求めよ
+ * @author hiroshi.kobayashi
+ * @create 2018-07-01
+ * @since
+ */
+public class D012Main {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+		System.out.print(Math.abs(N));
+    }
+}
