f4eee0884ad20e5b7e4d5ffa4dbcc926ca2aab05
hiroshi kobayashi <hiroshi.kobayashi1988@gmail.com>
2018-07-10 18:36:06 +0900

    Create D002.java

---
+++ src/com/d/D002.java
@@ -0,0 +1,23 @@
+package com.d;
+
+import java.util.Scanner;
+
+/**
+ *  数の比較
+ * @author hiroshi.kobayashi
+ * @create 2018-07-10
+ */
+public class D002Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b) {
+            System.out.println(a);
+        }else if (a < b){
+            System.out.println(b);
+        }else {
+            System.out.println("eq");
+        }
+    }
+}
