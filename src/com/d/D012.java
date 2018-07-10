d09ef0737090a64d7d55de87edf8a5a95f7e5533
hiroshi kobayashi <hiroshi.kobayashi1988@gmail.com>
2018-07-10 18:40:56 +0900

    Update D012.java

--- src/com/d/D012.java
+++ src/com/d/D012.java
@@ -6,9 +6,8 @@ import java.util.*;
  * 絶対値を求めよ
  * @author hiroshi.kobayashi
  * @create 2018-07-01
- * @since
  */
-public class D012Main {
+public class Main {
     public static void main(String args[] ) throws Exception {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
