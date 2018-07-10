8cd2066c1f70a28c61f3b90bbfc86ed139e19f2b
hiroshi kobayashi <hiroshi.kobayashi1988@gmail.com>
2018-07-10 18:05:14 +0900

    Create D004.java

---
+++ src/com/d/D004.java
@@ -0,0 +1,29 @@
+package com.d;
+
+import java.util.Scanner;
+
+/**
+ * 文字列の結合
+ * @author hiroshi.kobayashi
+ * @create 2018-07-01
+ * @since
+ */
+public class D004Main {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            String[] str = new String[20];
+            for (int i = 0; i < n; i++) {
+                    str[i] = sc.next();
+            }
+
+            System.out.print("Hello ");
+            for (int i = 0; i < n; i++) {
+                if (i == n - 1) {
+                    System.out.print(str[i] + ".");
+                }else {
+                    System.out.print(str[i] + ",");
+                }
+            }
+        }
+}
