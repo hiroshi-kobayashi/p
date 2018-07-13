9f00dcda04cfff1f264d8b20f9e744d6c0105295
hiroshi kobayashi <hiroshi.kobayashi1988@gmail.com>
2018-07-13 10:22:53 +0900

    Create C008.java

---
+++ src/com/c/C008.java
@@ -0,0 +1,34 @@
+package com.c;
+
+import java.util.Scanner;
+
+/**
+ * 文字列の抽出
+ * @author hiroshi.kobayashi
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String start = sc.next();
+        String end = sc.next();
+        String str = sc.next();
+        String str2 = str.substring(str.indexOf(start));
+        getContent(str2, start, end);
+    }
+
+    public static void getContent(String subStr, String start, String end){
+        if (subStr.contains(start) && subStr.contains(end)){
+            if (subStr.substring(subStr.indexOf(start) + start.length(), subStr.indexOf(end)).length() == 0){
+                System.out.println("<blank>");
+            }else {
+                System.out.println(subStr.substring(subStr.indexOf(start) + start.length(), subStr.indexOf(end)));
+            }
+            getContent(subStr.substring(subStr.indexOf(end) + end.length()), start, end);
+        }else {
+            return;
+        }
+    }
+
+}
