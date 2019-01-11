5313b7cc14a4e472bb6489dd1c6086018ea24d38
Hiro0831 <hiroshi.kobayashi1988@gmail.com>
2019-01-11 15:00:57 +0900

    Create D074.java

---
+++ src/com/d/D074.java
@@ -0,0 +1,20 @@
+package com.d;
+
+import java.util.Scanner;
+
+/**
+ * @author hiroshi.kobayashi
+ * @create 2018-03-04
+ * @since
+ * 時間の表記
+ */
+public class D074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int time = sc.nextInt();
+        System.out.println(time%24);
+    }
+}
+
+
+【Java】時間の表記のサンプルコード
